1046223428a5fa89f42950b6d954318d34db81ec
DSanchez <dsanbla@gmail.com>
2021-08-03 12:02:00 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 const password = "TrustNo1";
 let checkBoxes = document.querySelectorAll("input[type=checkbox]");
+let checkRanges = document.querySelectorAll("input[type=range]");
 let ranges = document.querySelectorAll("input[type=range]");
 let launchBtn = document.getElementById("launch");
+let rocket = document.querySelector(".rocket");
 
 function disables() {
     for (let i = 0; i < checkBoxes.length; i++) {
@@ -23,10 +25,39 @@ function enables() {
     for (let j = 0; j < ranges.length; j++) {
         ranges[j].disabled = false;
     }
-    launchBtn.disabled = false;
+}
+
+function checkEnables(){
+    let launchOk = 0;
+    for (let i = 0; i < checkBoxes.length; i++) {
+        checkBoxes[i].onchange = function(){
+            if (checkBoxes[i].checked) {
+                launchOk += 1;
+                console.log(launchOk);
+            }else{
+                launchOk -= 1;
+                console.log(launchOk);
+            }
+        };
+    }
+    let launchOkRanges = 0;
+    for(let i = 0; i<checkRanges.length; i++){
+        checkRanges[i].onchange = function(){
+            if (checkRanges[i].value == 100){
+                launchOkRanges += 1;
+                console.log(launchOkRanges);
+            }
+        }
+    }
+
+    if (launchOk === 6){
+        launchBtn.disabled = false;
+        console.log("ready");
+    }
 }
 
 disables();
+checkEnables();
 
 let ePass = document.getElementById("password");
 let checkPass = document.getElementById("unlock")
@@ -38,3 +69,36 @@ checkPass.addEventListener("click", function (e){
         ePass.value = "";
     }
 })
+
+let counter = 0;
+for (let i = 0; i < checkBoxes.length; i++) {
+    checkBoxes[i].addEventListener('change', (event) => {
+        if (event.currentTarget.checked) {
+            counter += 1;
+        } else {
+            counter -= 1;
+        }
+        if (counter == 6){
+            console.log("ready");
+        }
+    });
+}
+
+for (let i = 0; i < checkRanges.length; i++) {
+    checkRanges[i].addEventListener('change', (event) => {
+        if (event.currentTarget.value == 100) {
+            counter += 1;
+        } else {
+            counter -= 1;
+        }
+        if (counter == 11){
+            launchBtn.disabled = false;
+        }
+    });
+}
+
+launchBtn.addEventListener("click", function (){
+console.log("Launched");
+rocket.style.animationName = "launching";
+rocket.style.animationDuration = "3s";
+});
